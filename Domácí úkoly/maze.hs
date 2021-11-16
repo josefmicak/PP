43290aaf6211bab4a38c9362ee32948af4784a55
josefmicak <71832264+josefmicak@users.noreply.github.com>
2021-11-16 21:19:14 +0100

    Přidán druhý domácí úkol
    Přidán druhý domácí úkol - Components

--- Domácí úkoly/maze.hs
+++ Domácí úkoly/maze.hs
@@ -17,25 +17,25 @@ type Result = [String]
 pp :: Result -> IO ()
 pp x = putStr (concat (map (++"\n") x))
 
-maze :: Result -> String -> [String]--Hlavní funkce
+maze :: Result -> String -> Result--Hlavní funkce
 maze [] _ = [] 
-maze (inputString:inputStrings) (move:moves) = mazeAddDot (inputString:inputStrings) (getAllCoordinates (findStart sampleInput 0) (findStart sampleInput 0) ((move:moves) ++ getLastCharacter(move:moves))) 0
+maze (inputString:inputStrings) (move:moves) = mazeAddDot (inputString:inputStrings) (getAllCoordinates sampleInput (findStart sampleInput 0) (findStart sampleInput 0) ((move:moves) ++ getLastCharacter(move:moves))) 0
 
 getLastCharacter :: String -> String--Funkce získá poslední znak ze sekvence pohybů - zajistí provedení úplně posledního pohybu
 getLastCharacter (move:moves)
     | length(moves) == 0 = [move]
     | otherwise = getLastCharacter moves
 
-mazeAddDot :: Result -> [(Int,Int)] -> Int -> Result--Přidá tečky na všechny příslušné souřadnice
+mazeAddDot :: Result -> [(Int, Int)] -> Int -> Result--Přidá tečky na všechny příslušné souřadnice
 mazeAddDot [] _ _ = []
-mazeAddDot (inputString:inputStrings) ((coordinateCol,coordinateRow):rowsAndCols) stringId = mazeAddDotHelper inputString ((coordinateCol,coordinateRow):rowsAndCols) 0 stringId : mazeAddDot inputStrings ((coordinateCol,coordinateRow):rowsAndCols) (stringId+1)
+mazeAddDot (inputString:inputStrings) ((coordinateCol, coordinateRow):rowsAndCols) stringId = mazeAddDotHelper inputString ((coordinateCol, coordinateRow):rowsAndCols) 0 stringId : mazeAddDot inputStrings ((coordinateCol, coordinateRow):rowsAndCols) (stringId+1)
 
-mazeAddDotHelper :: String -> [(Int,Int)] -> Int -> Int -> String--Pomocná funkce pro mazeAddDot
+mazeAddDotHelper :: String -> [(Int, Int)] -> Int -> Int -> String--Pomocná funkce pro mazeAddDot
 mazeAddDotHelper [] _ _ _ = []
-mazeAddDotHelper (inputChar:inputChars) ((coordinateCol,coordinateRow):rowsAndCols) charId stringId
-    --K přidání tečky dojde pouze pokud A) dané souřadnice se nacházejí v uloženém seznamu souřadnic; B) na daných souřadnicích je prádzné místo (=>nepřekryjeme tečkou zeď, ani startovní pole)
-    | isCoordinateInList charId stringId ((coordinateCol,coordinateRow):rowsAndCols) == True && inputChar == ' ' = "." ++ mazeAddDotHelper inputChars ((coordinateCol,coordinateRow):rowsAndCols) (charId+1) stringId
-    | otherwise = inputChar : mazeAddDotHelper inputChars ((coordinateCol,coordinateRow):rowsAndCols) (charId+1) stringId
+mazeAddDotHelper (inputChar:inputChars) ((coordinateCol, coordinateRow):rowsAndCols) charId stringId
+    --K přidání tečky dojde pouze pokud A) dané souřadnice se nacházejí v uloženém seznamu souřadnic; B) na daných souřadnicích je prádzné místo (=> nepřekryjeme tečkou startovní pole)
+    | isCoordinateInList charId stringId ((coordinateCol, coordinateRow):rowsAndCols) == True && inputChar == ' ' = "." ++ mazeAddDotHelper inputChars ((coordinateCol, coordinateRow):rowsAndCols) (charId+1) stringId
+    | otherwise = inputChar : mazeAddDotHelper inputChars ((coordinateCol, coordinateRow):rowsAndCols) (charId+1) stringId
 
 findStart :: Result -> Int -> (Int, Int)--Zjistíme souřadnici startovního pole
 findStart (inputString:inputStrings) stringId
@@ -48,29 +48,41 @@ findStartCharacter startCharacter (inputChar:inputChars) charId
     | startCharacter == inputChar = charId
     | otherwise = findStartCharacter startCharacter inputChars (charId+1)
 
-getAllCoordinates :: (Int,Int) -> (Int,Int) -> String -> [(Int,Int)]--Získáme seznam všech souřadnic, na které se v poli během běhu programu přesuneme
-getAllCoordinates (startCol, startRow) (coordinateCol,coordinateRow) [] = []
-getAllCoordinates (startCol, startRow) (coordinateCol,coordinateRow) (move:moves)
+getAllCoordinates :: Result -> (Int, Int) -> (Int, Int) -> String -> [(Int, Int)]--Získáme seznam všech souřadnic, na které se v poli během běhu programu přesuneme
+getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol,coordinateRow) [] = []
+getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol,coordinateRow) (move:moves)
+    --Zjišťujeme, že bychom se daným pohybem dostali na souřadnice zdi - takový pohyb tedy nebereme v potaz
+    | move == 'd' && length(moves) > 1 && isWallOnCoordinates (inputString:inputStrings) (coordinateCol, coordinateRow+1) 0 = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow) moves
+    | move == 'u' && length(moves) > 1 && isWallOnCoordinates (inputString:inputStrings) (coordinateCol, coordinateRow-1) 0 = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow) moves
+    | move == 'l' && length(moves) > 1 && isWallOnCoordinates (inputString:inputStrings) (coordinateCol-1, coordinateRow) 0 = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow) moves
+    | move == 'r' && length(moves) > 1 && isWallOnCoordinates (inputString:inputStrings) (coordinateCol+1, coordinateRow) 0 = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow) moves
     --Zjišťujeme, že se jedná o souřadnice startovního pole - tyto souřadnice nejsou do seznamu přidány
-    | (coordinateCol,coordinateRow) == (startCol, startRow) && move == 'd' = getAllCoordinates (startCol, startRow) (coordinateCol,coordinateRow+1) moves
-    | (coordinateCol,coordinateRow) == (startCol, startRow) && move == 'u' = getAllCoordinates (startCol, startRow) (coordinateCol,coordinateRow-1) moves
-    | (coordinateCol,coordinateRow) == (startCol, startRow) && move == 'l' = getAllCoordinates (startCol, startRow) (coordinateCol-1,coordinateRow) moves
-    | (coordinateCol,coordinateRow) == (startCol, startRow) && move == 'r' = getAllCoordinates (startCol, startRow) (coordinateCol+1,coordinateRow) moves
-    --Zde se již nejedná o souřadnice startovního pole, tudíž jsou souřadnice do seznamu přidány
-    | move == 'd' = (coordinateCol,coordinateRow) : getAllCoordinates (startCol, startRow) (coordinateCol,coordinateRow+1) moves
-    | move == 'u' = (coordinateCol,coordinateRow) : getAllCoordinates (startCol, startRow) (coordinateCol,coordinateRow-1) moves
-    | move == 'l' = (coordinateCol,coordinateRow) : getAllCoordinates (startCol, startRow) (coordinateCol-1,coordinateRow) moves
-    | move == 'r' = (coordinateCol,coordinateRow) : getAllCoordinates (startCol, startRow) (coordinateCol+1,coordinateRow) moves
+    | move == 'd' && (coordinateCol, coordinateRow) == (startCol, startRow) = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow+1) moves
+    | move == 'u' && (coordinateCol, coordinateRow) == (startCol, startRow) = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow-1) moves
+    | move == 'l' && (coordinateCol, coordinateRow) == (startCol, startRow) = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol-1, coordinateRow) moves
+    | move == 'r' && (coordinateCol, coordinateRow) == (startCol, startRow) = getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol+1, coordinateRow) moves
+    --Zde se již nejedná o souřadnice startovního pole, a na souřadnicích se nenachází zeď, tudíž jsou souřadnice do seznamu přidány
+    | move == 'd' = (coordinateCol, coordinateRow) : getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow+1) moves
+    | move == 'u' = (coordinateCol, coordinateRow) : getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol, coordinateRow-1) moves
+    | move == 'l' = (coordinateCol, coordinateRow) : getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol-1, coordinateRow) moves
+    | move == 'r' = (coordinateCol, coordinateRow) : getAllCoordinates (inputString:inputStrings) (startCol, startRow) (coordinateCol+1, coordinateRow) moves
 
-getFirstCoordinate :: (Int,Int) -> Char -> (Int,Int)--Získá první set souřadnic (při absenci této funkce jsou do seznamu souřadnic chybně přidány souřadnice startovního pole)
-getFirstCoordinate (startCol, startRow) move
-    | move == 'd' = (startCol, startRow+1)
-    | move == 'u' = (startCol, startRow-1)
-    | move == 'l' = (startCol-1, startRow)
-    | move == 'r' = (startCol+1, startRow)
-
-isCoordinateInList :: Int -> Int -> [(Int,Int)] -> Bool--Zjistíme, jestli se na dané pole během běhu programu přesuneme - tzn. jestli jsou testované souřadnice v uloženém seznamu souřadnic
+isCoordinateInList :: Int -> Int -> [(Int, Int)] -> Bool--Zjistíme, jestli se na dané pole během běhu programu přesuneme - tzn. jestli jsou testované souřadnice v uloženém seznamu souřadnic
 isCoordinateInList _ _ [] = False
-isCoordinateInList charId stringId ((coordinateCol,coordinateRow):rowsAndCols)
+isCoordinateInList charId stringId ((coordinateCol, coordinateRow):rowsAndCols)
     | (charId == coordinateCol && stringId == coordinateRow) = True 
     | otherwise = isCoordinateInList charId stringId rowsAndCols
+
+isWallOnCoordinates :: Result -> (Int, Int) -> Int -> Bool--Funkce zjistí, jestli se na daných souřadnicích nenachází zeď (pokud ano, souřadnice se do seznamu nepřidají)
+isWallOnCoordinates [] _ _ = False
+isWallOnCoordinates (inputString:inputStrings) (coordinateCol, coordinateRow) stringId
+    | coordinateRow /= stringId = isWallOnCoordinates inputStrings (coordinateCol, coordinateRow) (stringId+1)
+    | isWallOnCoordinatesHelper inputString coordinateCol 0 == True = True
+    | otherwise = False
+
+isWallOnCoordinatesHelper :: String -> Int -> Int -> Bool--Pomocná funkce pro isWallOnCoordinates
+isWallOnCoordinatesHelper [] _ _ = False
+isWallOnCoordinatesHelper (inputChar:inputChars) coordinateCol charId 
+    | coordinateCol /= charId = isWallOnCoordinatesHelper inputChars coordinateCol (charId+1)
+    | inputChar == '*' = True 
+    | otherwise = False
